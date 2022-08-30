43948d0d63ae987875aaa71698c77e5da360daa0
Pablo Mazariegos <mazariegospablo928@gmail.com>
2022-08-30 15:33:33 -0600

    PyGame window

--- Paint/main.py
+++ Paint/main.py
@@ -1,1 +1,15 @@
+import sys
+
+import pygame
+
+pygame.init()
+
+size = (1550, 800)
+
+screen = pygame.display.set_mode(size)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
 
